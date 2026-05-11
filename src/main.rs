5e00b00fb1ad8aa135ded755b03507a4b2feaa44
Kai Parsons <thmigu.kp@gmail.com>
2026-05-10 23:32:49 -0700

    feat: made window color rainbow

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use sdl3::{event::Event, pixels::Color};
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
 	let sdl_context = sdl3::init().unwrap();
@@ -22,16 +24,31 @@ fn main() {
 		.event_pump()
 		.unwrap();
 
+	let mut frames: u128 = 0;
+
 	'running: loop {
+		frames += 1;
+
+		let t = frames as f32 * 0.02;
+
+		let r = ((t.sin() * 0.5 + 0.5) * 255.0) as u8;
+		let g = (((t + 2.094).sin() * 0.5 + 0.5) * 255.0) as u8;
+		let b = (((t + 4.188).sin() * 0.5 + 0.5) * 255.0) as u8;
+
+		canvas.set_draw_color(Color::RGB(r, g, b));
+		canvas.clear();
+
 		for event in event_pump.poll_iter() {
 			match event {
 				Event::Quit {
 					..
-				} => {
-					break 'running;
-				}
+				} => break 'running,
 				_ => {}
 			}
 		}
+
+		canvas.present();
+
+		sleep(Duration::from_millis(1000 / 60));
 	}
 }
