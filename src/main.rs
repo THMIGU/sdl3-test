d348a9cac0e235ea937712622b1e50cfa2d09c56
Kai Parsons <thmigu.kp@gmail.com>
2026-05-10 21:13:10 -0700

    feat: window loading code from the docs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use sdl3::{event::Event, pixels::Color};
+
 fn main() {
-    println!("Hello, world!");
+	let sdl_context = sdl3::init().unwrap();
+	let video_subsystem = sdl_context
+		.video()
+		.unwrap();
+
+	let window = video_subsystem
+		.window("sdl3-test", 800, 600)
+		.position_centered()
+		.build()
+		.unwrap();
+
+	let mut canvas = window.into_canvas();
+
+	canvas.set_draw_color(Color::RGB(255, 0, 0));
+	canvas.clear();
+	canvas.present();
+
+	let mut event_pump = sdl_context
+		.event_pump()
+		.unwrap();
+
+	'running: loop {
+		for event in event_pump.poll_iter() {
+			match event {
+				Event::Quit {
+					..
+				} => {
+					break 'running;
+				}
+				_ => {}
+			}
+		}
+	}
 }
